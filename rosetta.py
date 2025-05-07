bfdb95ac8c1a83d477cae107b070df4b5454fe07
Pablo Rodríguez Belenguer <parodbe@gmail.com>
2025-05-07 12:03:21 +0200

    Update rosetta.py

--- rosetta.py
+++ rosetta.py
@@ -1,46 +1,78 @@
-# Importing Streamlit
 import streamlit as st
 
-# Define HTML and CSS for the tooltip
-html_content = """
-<style>
-.tooltip {
-  position: relative;
-  display: inline-block;
-  cursor: pointer;
-}
+# Function to display the image on hover
+def display_image_on_hover(image_url, i):
+    # Generate unique class names for each image
+    hover_class = f'hoverable_{i}'
+    tooltip_class = f'tooltip_{i}'
+    image_popup_class = f'image-popup_{i}'
 
-.tooltip .tooltiptext {
-  visibility: hidden;
-  width: 120px;
-  background-color: black;
-  color: white;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px 0;
-  position: absolute;
-  z-index: 1;
-  top: 100%;
-  left: 50%;
-  margin-left: -60px;
-  opacity: 0;
-  transition: opacity 0.3s;
-}
+    # Define the unique CSS for each image
+    hover_css = f'''
+        .{hover_class} {{
+            position: relative;
+            display: inline-block;
+            cursor: pointer;
+        }}
+        .{hover_class} .{tooltip_class} {{
+            opacity: 0;
+            position: absolute;
+            bottom: 100%;
+            left: 50%;
+            transform: translateX(-50%);
+            transition: opacity 0.5s;
+            background-color: rgba(0, 0, 0, 0.8);
+            color: #fff;
+            padding: 4px;
+            border-radius: 4px;
+            text-align: center;
+            white-space: nowrap;
+        }}
+        .{hover_class}:hover .{tooltip_class} {{
+            opacity: 1;
+        }}
+        .{image_popup_class} {{
+            position: absolute;
+            display: none;
+            background-image: none;
+            width: 200px;
+            height: 200px;
+        }}
+        .{hover_class}:hover .{image_popup_class} {{
+            display: block;
+            background-image: url({image_url});
+            background-size: cover;
+            z-index: 999;
+        }}
+    '''
+    tooltip_css = f"<style>{hover_css}</style>"
 
-.tooltip:hover .tooltiptext {
-  visibility: visible;
-  opacity: 1;
-}
-</style>
+    # Define the html for each image
+    image_hover = f'''
+        <div class="{hover_class}">
+            <a href="{image_url}">{image_url}</a>
+            <div class="{tooltip_class}">Image {i}</div>
+            <div class="{image_popup_class}"></div>
+        </div>
+    '''
+    
+    # Write the dynamic HTML and CSS to the content container
+    st.markdown(f'<p>{image_hover}{tooltip_css}</p>', unsafe_allow_html=True)
 
-<div class="tooltip">
-  <img src="https://cdn.vox-cdn.com/thumbor/WR9hE8wvdM4hfHysXitls9_bCZI=/0x0:1192x795/1400x1400/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg" alt="Image" style="width:200px;height:auto;">
-  <span class="tooltiptext">Tooltip text</span>
-</div>
-"""
+# Initialize Streamlit app
+st.title("Floating Image Tooltips on MouseOver")
 
-# Display HTML in Streamlit
-st.markdown(html_content, unsafe_allow_html=True)
+# A list of image urls
+url_list = [
+    "https://everydayswag.org/images/misc/ableton-sampler-vol-env.png",
+    "https://everydayswag.org/images/email.png"
+]
+
+# Create a container for the content that triggers the tooltip on mouseover
+with st.expander("⛓", expanded=True):
+    # Generate the dynamic HTML and CSS for each URL in the list
+    for i, url in enumerate(url_list):
+        display_image_on_hover(url, i)
 
 
 # import streamlit as st
